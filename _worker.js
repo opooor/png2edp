fefa5915bc5a3a47e2e571ff037e5e59c4953bbd
Hym <105768239+opooor@users.noreply.github.com>
2026-06-06 13:19:28 +0800

    Delete _worker.js

--- _worker.js
+++
@@ -1,8 +0,0 @@
-export default {
-  async fetch(){
-    const CANVA = "https://hymmaker.my.canvasite.cn/";
-    const api = `https://api.screenshotmachine.com?key=demo&url=${encodeURIComponent(CANVA)}&dimension=1024x758`;
-    const r = await fetch(api);
-    return new Response(await r.blob(),{headers:{"Content-Type":"image/png","Cache-Control":"no-cache"}})
-  }
-}
