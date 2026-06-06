831c953f3a56b121bc5021bc5f01542f8b68afbc
Hym <105768239+opooor@users.noreply.github.com>
2026-06-06 13:38:55 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,18 +1,17 @@
 export default {
   async fetch(request) {
-    const CANVA = "https://hymmaker.my.canvasite.cn/";
-    const url = new URL(request.url);
-    // /proxy 路由用来中转可画
-    if(url.pathname === "/proxy"){
-      const resp = await fetch(CANVA,{redirect:"follow"});
-      const headers = new Headers(resp.headers);
-      headers.delete("content-security-policy");
+    const CANVA_URL = "https://hymmaker.my.canvasite.cn/";
+    const { pathname } = new URL(request.url);
+
+    // 代理路由：只/proxy转发目标网站
+    if(pathname === "/proxy"){
+      const res = await fetch(CANVA_URL, {redirect:"follow"});
+      const headers = new Headers(res.headers);
       headers.delete("x-frame-options");
-      return new Response(await resp.text(),{headers});
+      headers.delete("content-security-policy");
+      return new Response(res.body, {headers});
     }
-    // 首页返回html截图页面
-    return new Response(await fetch(new Request(`${url.origin}/index.html`)).then(r=>r.text()),{
-      headers:{"Content-Type":"text/html;charset=utf-8"}
-    })
+    // 其余所有路径：直接读取静态index.html，不再fetch自身
+    return await fetch(request);
   }
 }
